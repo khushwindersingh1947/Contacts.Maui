63e360257f37bc04d0d32c90d50e3a90bc94cb3f
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-08-17 18:54:22 -0400

    created models and static storage class

---
+++ Contacts.Maui/Models/ContactRepository.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Maui.Models
+{
+    public static class ContactRepository
+    {
+        public static List<Contact> _contacts = new List<Contact>() 
+        {
+            new Contact { ContactId = 1, Name="John Doe", Email="aoe@email.com"},
+            new Contact {ContactId = 2, Name="Jahn Aoe", Email="boe@email.com"},
+            new Contact {ContactId = 3, Name="Jehn Boe", Email="coe@email.com"},
+            new Contact {ContactId = 4, Name="Jrhn Coe", Email="doe@email.com"}
+        };
+
+        public static List<Contact> GetContacts() => _contacts;
+
+        public static Contact GetContactById(int id) 
+        {
+            var contact = _contacts.FirstOrDefault(x => x.ContactId == id);
+            if (contact != null) 
+            {
+                return new Contact()
+                {
+                    ContactId = contact.ContactId,
+                    Name = contact.Name,
+                    Email = contact.Email,
+                    Address = contact.Address,
+                    Phone = contact.Phone
+                };
+            }
+            return contact;
+        }
+
+        public static void UpdateContact(int contactId, Contact contact)
+        {
+            if (contactId != contact.ContactId) return;
+
+            var contactToUpdate = _contacts.FirstOrDefault(x => x.ContactId == contactId);
+
+            //can also use automapper
+            if (contactToUpdate != null) 
+            {
+                contactToUpdate.Address = contact.Address;
+                contactToUpdate.Email = contact.Email;
+                contactToUpdate.Name = contact.Name;
+                contactToUpdate.Phone = contact.Phone;
+            }
+
+
+        }
+
+
+    };
+    
+}
